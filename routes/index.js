bcbc8efa718edae0ab3d899fbd46f09c7005f7cf
Mateusz Pawlowski <mpawlowski5467@gmail.com>
2024-05-20 15:51:47 -0500

    update

--- routes/index.js
+++ routes/index.js
@@ -11,15 +11,23 @@ router.get('/', function(req, res, next) {
       console.log(err);
       res.render('error');
     }
-    res.render('index', {allrecs: result });
-  }); 
+
+    let query = "SELECT promotion_id, promotitle, promoimage FROM promotion WHERE startdate <= CURRENT_DATE() and enddate >= CURRENT_DATE()";
+
+db.query(query, (err, results2) => {
+  if (err) {
+    console.log(err);
+    res.render('error');
+  }
+    res.render('index', {allrecs: result, promos: results2 });
+});
+});
+});
+
 
 //   var currenttime = Date(Date.now()).toString();
 //   var passeddata = "Mateusz Pawlowski "
 // res.render('index', { currenttime: currenttime, passeddata: passeddata});
-});
-
-
 
 
 
