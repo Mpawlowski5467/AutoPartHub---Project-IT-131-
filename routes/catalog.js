5b14be3b426bb4cf8fe8674980174c7e62309b6a
Mateusz Pawlowski <mpawlowski5467@gmail.com>
2024-05-29 19:57:00 -0500

    update

--- routes/catalog.js
+++ routes/catalog.js
@@ -20,29 +20,28 @@ res.render('catalog', {allrecs: result });
 // ==================================================
 router.post('/add', function(req, res, next) {
     if (typeof req.session.cart !== 'undefined' && req.session.cart ) {
-    if (req.session.cart.includes(req.body.Product_ID))
-    {
-    // Item Exists in Basket - Increase Quantity
-    var n = req.session.cart.indexOf(req.body.Product_ID);
-    req.session.qty[n] = parseInt(req.session.qty[n]) +
-    parseInt(req.body.qty);
-    }
-    else
-    {
-    // Item Being Added First Time
-    req.session.cart.push(req.body.Product_ID);
-    req.session.qty.push(req.body.qty);
-    }
-    }else {
-    var cart = [];
-    cart.push(req.body.Product_ID);
-    req.session.cart = cart;
-    var qty = [];
-    qty.push(req.body.qty);
-    req.session.qty = qty;
+        if (req.session.cart.includes(req.body.Product_ID))
+            {
+            // Item Exists in Basket - Increase Quantity
+            var n = req.session.cart.indexOf(req.body.Product_ID);
+            req.session.qty[n] = parseInt(req.session.qty[n]) + parseInt(req.body.qty);
+            }
+        else
+            {
+            // Item Being Added First Time
+            req.session.cart.push(req.body.Product_ID);
+            req.session.qty.push(req.body.qty);
+            }
+    } else {
+        var cart = [];
+        cart.push(req.body.Product_ID);
+        req.session.cart = cart;
+        var qty = [];
+        qty.push(req.body.qty);
+        req.session.qty = qty;
     }
     res.redirect('/catalog/cart');
-    });
+});
 
 // ==================================================
 // Route to show shopping cart
@@ -51,14 +50,19 @@ router.get('/cart', function(req, res, next) {
     if (!Array.isArray(req.session.cart) || !req.session.cart.length){
     res.render('cart', {cartitems: 0 });
     } else {
-    let query = "SELECT Product_ID, Supplier_ID, Productname , ItemSold , RetailPrice , Packaging , IsDiscontinued FROM ProductInfo WHERE ID IN (" + req.session.cart + ") order by find_in_set(Product_ID, '" + req.session.cart + "');";
+
+        
+    let query = "SELECT Product_ID, Supplier_ID, Productname , ItemSold , RetailPrice , Packaging , IsDiscontinued FROM ProductInfo WHERE Product_ID IN (" + req.session.cart + ") order by find_in_set(Product_ID, '" + req.session.cart + "');";
     // execute query
     db.query(query, (err, result) => {
-    if (err) {res.render('error');} else
-    {res.render('cart', {cartitems: result, qtys:
-    req.session.qty });}
+        if (err) {
+            console.log(err);
+            res.render('error');
+        } else{
+            res.render('cart', {cartitems: result, qtys: req.session.qty });
+        }
     });
     }
-    });
+ });
 
 module.exports = router;
